1be76a1b7a38849b12dbb497128445c820cc5d08
sasidhar <sasidhar.678@gmail.com>
2016-05-16 18:08:18 +0530

    fixes :
    reports, middleware support for jwt auth and projects & reports related user

---
+++ middlewares/middleware.js
@@ -0,0 +1,10 @@
+/**
+ * Created by SASi on 16-May-16.
+ */
+
+module.exports = (function () {
+    const api_middleware = function (req, res, next) {
+
+    };
+    return ({api_middleware: api_middleware});
+})();
